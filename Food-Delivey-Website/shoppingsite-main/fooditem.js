a1a88c97f3814405f2d9c791f7eae3c64c59fa7e
Ishaa1234 <109645313+Ishaa1234@users.noreply.github.com>
2022-11-26 11:19:19 +0530

    Add files via upload

---
+++ Food-Delivey-Website/shoppingsite-main/fooditem.js
@@ -0,0 +1,341 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Dum biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'images/dumbiryani.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'hyderabadi biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/hydrechic.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Muradabadi',
+    category : 'biryani',
+    rating : 4.3,
+    price: 18,
+    img: 'images/muradabadi.jpg',
+    quantity: 1
+},
+
+  {
+    id: 4,
+    name: 'Kashmiri Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 13,
+    img: 'images/biryani/kashmiri.pulao.jpg',
+    quantity: 1
+},
+{
+    id: 5,
+    name: 'Memoni Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 20,
+    img: 'images/biryani/memonibiryani.png',
+    quantity: 1
+},
+{
+    id: 6,
+    name: 'Mughlai Biryani',
+    category : 'biryani',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/mughlai-biryani.jpg',
+    quantity: 1
+},
+{
+    id: 10,
+    name: 'Garlic Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 11,
+    img: 'images/garlicchic.jpg',
+    quantity: 1
+},
+{
+    id: 11,
+    name: 'Chilly Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 10,
+    img: 'images/chillychic.jpg',
+    quantity: 1
+},
+
+{
+    id: 14,
+    name: 'Tandoori',
+    category : 'chicken',
+    rating : 4.3,
+    price: 17,
+    img: 'images/tandoori.jpg',
+    quantity: 1
+},
+{
+    id: 12,
+    name: 'Chicken 65',
+    category : 'chicken',
+    rating : 4.3,
+    price: 14,
+    img: 'images/chic65.jpg',
+    quantity: 1
+},
+{
+    id: 13,
+    name: 'Fried Chicken',
+    category : 'chicken',
+    rating : 4.3,
+    price: 12,
+    img: 'images/fried.jpg',
+    quantity: 1
+},
+{
+    id: 14,
+    name: 'Lollipop',
+    category : 'chicken',
+    rating : 4.3,
+    price: 17,
+    img: 'images/loli.jpg',
+    quantity: 1
+},
+
+
+{
+    id: 16,
+    name: 'Mewari',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'images/mewari.jpg',
+    quantity: 1
+
+},
+{
+    id: 17,
+    name: 'Paneer Tikka',
+    category : 'paneer',
+    rating : 4.3,
+    price: 10,
+    img: 'images/tikka.jpg',
+    quantity: 1
+},
+{
+    id: 18,
+    name: 'Matar Paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 15,
+    img: 'images/matarpan.jpg',
+    quantity: 1
+
+},
+{
+    id: 19,
+    name: 'Kadai paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 12,
+    img: 'images/kadai.jpg',
+    quantity: 1
+
+},
+{
+    id: 20,
+    name: 'Paneer masala',
+    category : 'paneer',
+    rating : 4.3,
+    price: 8,
+    img: 'images/paneermas.jpg',
+    quantity: 1
+},
+
+
+{
+    id: 21,
+    name: 'Shahi paneer',
+    category : 'paneer',
+    rating : 4.3,
+    price: 7,
+    img: 'images/shahi.jpg',
+    quantity: 1
+},
+
+{
+    id: 23,
+    name: 'Bhindi Masala',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 8,
+    img: 'images/bhindi.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Veg Kolhapuri',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'images/kolha.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: ' Mix Veggies',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 5,
+    img: 'images/vef.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Stuffed Brinjal',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 7,
+    img: 'images/brin.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Baby corn',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 10,
+    img: 'images/corn.jpg',
+    quantity: 1
+},
+{
+    id: 28,
+    name: 'Veg kadai',
+    category : 'vegetable',
+    rating : 4.3,
+    price: 4,
+    img: 'images/vegkadai.jpg',
+    quantity: 1
+},
+{
+    id: 29,
+    name: 'Spring rolls',
+    category : 'chinese',
+    rating : 4.3,
+    price: 4,
+    img: 'images/springroll.jpg',
+    quantity: 1
+},
+{
+    id: 23,
+    name: 'Chicken rice',
+    category : 'chinese',
+    rating : 4.3,
+    price: 8,
+    img: 'images/chickenrice.jpg',
+    quantity: 1
+},
+{
+    id: 24,
+    name: 'Chilly Potato',
+    category : 'chinese',
+    rating : 4.3,
+    price: 7,
+    img: 'images/chillypotato.jpg',
+    quantity: 1
+},
+{
+    id: 25,
+    name: ' Chicken Hakka Noddles',
+    category : 'chinese',
+    rating : 4.3,
+    price: 5,
+    img: 'images/chotuchinese.jpg',
+    quantity: 1
+},
+{
+    id: 26,
+    name: 'Hakka Noddles',
+    category : 'chinese',
+    rating : 4.3,
+    price: 7,
+    img: 'images/hakka.jpg',
+    quantity: 1
+},
+{
+    id: 27,
+    name: 'Schezwan Noddles',
+    category : 'chinese',
+    rating : 4.3,
+    price: 10,
+    img: 'images/sch.jpg',
+    quantity: 1
+},
+
+
+{
+    id: 37,
+    name: 'Idli',
+    category : 'south indian',
+    rating : 4.3,
+    price: 18,
+    img: 'images/dosa.jpg',
+    quantity: 1
+},
+
+{
+    id: 39,
+    name: 'Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 12,
+    img: 'images/idli.jpg',
+    quantity: 1
+},
+{
+    id: 40,
+    name: 'Upma',
+    category : 'south indian',
+    rating : 4.3,
+    price: 10,
+    img: 'images/upma.jpg',
+    quantity: 1
+},
+{
+    id: 41,
+    name: 'Masala Dosa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 15,
+    img: 'images/masala.jpg',
+    quantity: 1
+},
+{
+    id: 42,
+    name: 'Meduwada',
+    category : 'south indian',
+    rating : 4.3,
+    price: 40,
+    img: 'images/meduwada.jpg',
+    quantity: 1
+},
+{
+    id: 43,
+    name: 'Uthappa',
+    category : 'south indian',
+    rating : 4.3,
+    price: 25,
+    img: 'images/uthappa.jpg',
+    quantity: 1
+},
+]
+
+export {foodItem};
